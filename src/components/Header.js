58e91c81805d346be316a9d7e5d2dac3fdf60ee5
stevendevcoder <stevensernazuluaga@gmail.com>
2022-04-05 14:25:12 -0500

    Creacion del proyecto

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../css/header.css';
+
+function Header () {
+    return(
+        <nav>
+            <h1>Caloric App</h1>
+            <ul>
+                <li>Meńu</li>
+                <li>Mis Requerimientos</li>
+                <li>Comidas</li>
+                <li>Macronutrientes</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Header;
