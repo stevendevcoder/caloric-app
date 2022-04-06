1e9c4ce2bdead8a40d0dd5df9116cde69ac785bd
Krb0 <maurodavidcaceres@gmail.com>
2022-04-06 16:41:08 -0300

    routes added

---
+++ src/components/Macronutrientes.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Macronutrientes = () => {
+  return <div>Macronutrientes</div>;
+};
+
+export default Macronutrientes;
---
+++ src/components/Requerimientos.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Requerimientos = () => {
+  return <div>Requerimientos</div>;
+};
+
+export default Requerimientos;
